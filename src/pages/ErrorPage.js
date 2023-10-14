3d78ad27314eb3648e535cd07454ccfc7720cf42
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-10-14 22:49:55 +0700

    page content

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,14 +1,27 @@
 import React from "react";
-import MainNavigation from "../component/MainNavigation";
+import { useRouteError } from "react-router-dom";
+import PageContent from "../component/PageContent";
 
 function ErrorPage() {
+  const error = useRouteError();
+
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
   return (
     <>
-      <MainNavigation />
-      <main>
-        <h1>An Error has Occured!!</h1>
-        <p>Could not find this page</p>
-      </main>
+      <PageContent title={title}>
+        <p>{message}</p>
+      </PageContent>
     </>
   );
 }
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,7 +2,11 @@ import { useLoaderData } from "react-router-dom";
 import ProductList from "../component/ProductList";
 
 function ProductsPage() {
-  const items = useLoaderData();
+  const data = useLoaderData();
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+  const items = data.items;
   return <ProductList products={items} />;
 }
 
@@ -12,8 +16,11 @@ export async function loader() {
   const response = await fetch("https://website-commerce-default-rtdb.firebaseio.com/products.json");
 
   if (!response.ok) {
+    return { isError: true, message: "could not fetch the product" };
+    // throw { message: "could not fetch the product" };
   } else {
-    const resData = await response.json();
-    return resData.items;
+    // const resData = await response.json();
+    // return resData.items;
+    return response;
   }
 }
