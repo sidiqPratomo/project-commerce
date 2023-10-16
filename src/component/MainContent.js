ec1b5ebb61c99dcfe4513e4c029306b2c6ae28fc
pratomosidiq123 <pratomosidiq123@gmail.com>
2023-10-16 22:19:29 +0700

    add content

--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -4,11 +4,18 @@ import classes from "./MainContent.module.css";
 function MainContent() {
   return (
     <div className={classes.container}>
-      <div>
-        <h1>maincontent</h1>
+      <div className={classes.kiri}>
+        <div className={classes.konten}>
+          <h3>HAT - HANDBAG - SHOES - EYEWEAR</h3>
+          <h1>Accessories for her</h1>
+          <p>Consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore</p>
+          <div>
+            <button className={classes.btn}>SHOP NOW</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <h1> maincontent</h1>
+      <div className={classes.kanan}>
+        <img src="https://pbs.twimg.com/media/DapBEuMVQAAP44V?format=jpg&name=900x900" alt="gambar-kanan" />
       </div>
     </div>
   );
